37ed723f43fd60a56979b0435be35b5c6556bb68
BrandonHosang <brandonhosang@yahoo.com>
2022-03-23 01:08:19 -0400

    Fixed portal exiting angle

--- Assets/Scripts/Portals/Portal.cs
+++ Assets/Scripts/Portals/Portal.cs
@@ -43,12 +43,18 @@ public class Portal : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            GameObject player = other.gameObject;
             player.GetComponent<CharacterController>().enabled = false;
-            player.transform.position = oppositePortal.GetComponentInChildren<Transform>().transform.position;
-            player.GetComponent<CharacterController>().enabled = true;
+            player.transform.position = new Vector3(oppositePortal.transform.position.x, oppositePortal.transform.position.y - 1, oppositePortal.transform.position.z);
+
+            float entranceAngle;
+            Vector3 normal = gameObject.transform.forward;
+            entranceAngle = Mathf.Acos(Vector3.Dot(normal, -player.transform.forward) / (Vector3.Magnitude(normal) * Vector3.Magnitude(-player.transform.forward)));
+            entranceAngle *= Mathf.Rad2Deg;
+
             player.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, player.transform.eulerAngles.z);
-            player.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, oppositePortal.transform.eulerAngles.y, player.transform.eulerAngles.z);
+            player.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, oppositePortal.transform.eulerAngles.y + entranceAngle, player.transform.eulerAngles.z);
+            player.GetComponent<CharacterController>().enabled = true;
             StartCoroutine(Cooldown());
         }
     }
